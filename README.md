8ba0c28152a59ceb6bb7d8dc677386316003ba01
pondichys <sebastien.pondichy@gmail.com>
2022-07-13 08:58:50 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Windows setup using chocolatey
+
+This repository contains a simple Powershell script that uses chocolatey to setup the various tools I want to be available on my Windows workstation.
+
+## Requirements
+
+- a Microsoft Windows workstation.
+- chocolatey installed. See the installation instructions for a personal use [here](https://chocolatey.org/install#individual).
+
+## Usage
+
+Clone or download this repository and run the file [choco-setup-win.ps1](choco-setup-win.ps1) in an elevated powershell prompt.
---
+++ choco-setup-win.ps1
@@ -0,0 +1,46 @@
+# Standard tools
+choco install 7zip -y
+choco install bitwarden -y
+choco install bitwarden-cli -y
+choco install calibre -y
+choco install chocolateygui -y
+choco install docker-desktop -y
+choco install eac -y
+choco install ferdi -y
+choco install gh -y
+choco install git -y
+choco instyall handbrake -y
+choco install jq -y
+choco install makemv -y
+choco install notion -y
+choco install openssh -y
+choco install postman -y
+choco install starship -y
+choco install vlc -y
+choco install xmedia-recode -y
+choco install yq -y
+
+# Cloud CLIs
+choco install awscli -y
+choco install azure-cli -y
+choco install scaleway-cli -y
+
+# Hashicorp tools
+choco install packer -y
+choco install terraform -y
+choco install vagrant -y
+
+# Terraform tools
+choco install terraform-docs -y
+choco install tflint -y
+
+# Kubernetes tools
+choco install argocd-cli -y
+choco install k3d -y
+choco install k9s -y
+choco install kubectx -y
+choco install kubens -y
+choco install kubernetes-cli -y
+choco install kubernetes-helm -y
+choco install lens -y
+
